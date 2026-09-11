48320adbe0a27c9ea4e95b2812f9ea6a7a145c35
projectofwang <projectofwang@gmail.com>
2026-09-11 21:07:27 +0700

    style: remove Nix comments

--- modules/nixos/graphics.nix
+++ modules/nixos/graphics.nix
@@ -1,8 +1,6 @@
 { ... }:
 
 {
-  # Shared graphics capability for profiles that need accelerated Wayland,
-  # Vulkan, video decode, or 32-bit graphics runtimes.
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
