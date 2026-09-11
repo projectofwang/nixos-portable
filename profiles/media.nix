c6674cd351b23b65e55b0cb9bfdfe779ce2e8e68
projectofwang <projectofwang@gmail.com>
2026-09-11 19:11:04 +0700

    style(media): use nixfmt function application layout

--- profiles/media.nix
+++ profiles/media.nix
@@ -2,11 +2,10 @@
 
 {
   # Optional entertainment stack. It is intentionally not part of desktop.
-  nixpkgs.config.allowUnfreePredicate =
-    pkg:
-      builtins.elem (lib.getName pkg) [
-        "stremio-linux-shell"
-      ];
+  nixpkgs.config.allowUnfreePredicate = (pkg: builtins.elem
+    (lib.getName pkg) [
+      "stremio-linux-shell"
+    ]);
 
   home-manager.users.${username}.home.packages = with pkgs; [
     mpv
