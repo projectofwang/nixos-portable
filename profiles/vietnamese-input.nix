4afc25efc68efc2a302371e11c1c56d6a6d8891d
projectofwang <projectofwang@gmail.com>
2026-09-11 13:58:50 +0700

    refactor(lotus): consume upstream NixOS module and package

--- profiles/vietnamese-input.nix
+++ profiles/vietnamese-input.nix
@@ -1,11 +1,24 @@
-{ ... }:
+{ inputs, pkgs, ... }:
 
 {
   imports = [
-    ../modules/nixos/input-method
+    inputs.fcitx5-lotus.nixosModules.fcitx5-lotus
   ];
+
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5 = {
+      waylandFrontend = true;
+      addons = [
+        inputs.fcitx5-lotus.packages.${pkgs.stdenv.hostPlatform.system}.fcitx5-lotus
+        pkgs.fcitx5-gtk
+      ];
+    };
+  };
 
   services.fcitx5-lotus = {
     enable = true;
+    package = inputs.fcitx5-lotus.packages.${pkgs.stdenv.hostPlatform.system}.fcitx5-lotus;
   };
 }
