91586842a9cb549c02717041cdc52dec728326d6
projectofwang <projectofwang@gmail.com>
2026-09-11 21:05:13 +0700

    style: remove Nix comments

--- hosts/machine/boot.nix
+++ hosts/machine/boot.nix
@@ -1,8 +1,6 @@
 { ... }:
 
 {
-  # Default for modern UEFI systems.
-  # If the target machine uses a different boot setup, change this file only.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 }
