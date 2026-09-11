3cd871ac831024b923575923e8a758e309fc1630
projectofwang <projectofwang@gmail.com>
2026-09-11 21:07:03 +0700

    style: remove Nix comments

--- modules/nixos/core/users.nix
+++ modules/nixos/core/users.nix
@@ -1,8 +1,6 @@
 { pkgs, username, ... }:
 
 {
-  # Zsh is managed by Home Manager, but NixOS must enable it system-wide
-  # before it can be used as a login shell.
   programs.zsh.enable = true;
 
   users.users.${username} = {
@@ -14,7 +12,5 @@
     ];
   };
 
-  # No password is declared here. Set the initial password during installation
-  # or with `passwd` on an existing system; it persists across rebuilds.
   security.sudo.wheelNeedsPassword = true;
 }
