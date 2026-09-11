b41e4cf509091106679a09c0e0fdf17e2fa23ae1
projectofwang <projectofwang@gmail.com>
2026-09-11 12:48:46 +0700

    refactor(lib): extract reusable NixOS host builder

---
+++ lib/mk-host.nix
@@ -0,0 +1,40 @@
+{ inputs, lib, home-manager, profiles }:
+
+{
+  mkHost =
+    {
+      machine,
+      hostModule,
+    }:
+    let
+      selectedProfiles = profiles.validate machine.profiles;
+      home = import ./home-manager.nix { inherit inputs; };
+    in
+    lib.nixosSystem {
+      inherit (machine) system;
+
+      specialArgs = {
+        inherit inputs machine;
+        hostname = machine.hostname;
+        username = machine.username;
+        timeZone = machine.timeZone;
+        nixosStateVersion = machine.nixosStateVersion;
+        homeStateVersion = machine.homeStateVersion;
+      };
+
+      modules = [
+        hostModule
+        home-manager.nixosModules.home-manager
+      ]
+      ++ map (profile: ../profiles/${profile}.nix) selectedProfiles
+      ++ [
+        {
+          system.stateVersion = machine.nixosStateVersion;
+          home-manager = home.mkHome {
+            username = machine.username;
+            homeStateVersion = machine.homeStateVersion;
+          };
+        }
+      ];
+    };
+}
