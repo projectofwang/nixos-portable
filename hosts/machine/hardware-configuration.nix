80ea3bb27c59355395129f54675a1df67d7c3d68
projectofwang <projectofwang@gmail.com>
2026-09-11 21:05:20 +0700

    style: remove Nix comments

--- hosts/machine/hardware-configuration.nix
+++ hosts/machine/hardware-configuration.nix
@@ -1,11 +1,4 @@
-# Replace this file with the hardware-configuration.nix generated on the target machine.
-#
-# Run on the new machine:
-#   sudo nixos-generate-config --show-hardware-config > /tmp/hardware-configuration.nix
-# Then copy the generated file here.
-
 { ... }:
 
 {
-  # Placeholder only. Do not build this template before replacing this file.
 }
