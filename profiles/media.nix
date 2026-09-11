5c2121ef301631eb8e74e40f537c282f4cb6a866
projectofwang <projectofwang@gmail.com>
2026-09-11 19:17:13 +0700

    fix(media): use additive unfree package option

--- profiles/media.nix
+++ profiles/media.nix
@@ -1,10 +1,10 @@
-{ username, lib, pkgs, ... }:
+{ username, pkgs, ... }:
 
 {
   # Optional entertainment stack. It is intentionally not part of desktop.
-  /*nixfmt:disable*/
-  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [ "stremio-linux-shell" ];
-  /*nixfmt:enable*/
+  nixpkgs.config.allowUnfreePackages = [
+    "stremio-linux-shell"
+  ];
 
   home-manager.users.${username}.home.packages = with pkgs; [
     mpv
