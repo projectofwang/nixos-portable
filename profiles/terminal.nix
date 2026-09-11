eac36461484067529769aebc06c6421e8f5620cc
projectofwang <projectofwang@gmail.com>
2026-09-11 21:09:19 +0700

    style: remove profile comments

--- profiles/terminal.nix
+++ profiles/terminal.nix
@@ -1,7 +1,6 @@
 { username, ... }:
 
 {
-  # Optional graphical terminal stack. User-level configuration stays in Home Manager.
   home-manager.users.${username}.imports = [
     ../modules/home-manager/terminal.nix
   ];
