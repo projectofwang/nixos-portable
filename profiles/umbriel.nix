e2172146b82d7da996bf9ba9002d74b69d90f187
projectofwang <projectofwang@gmail.com>
2026-09-11 21:09:27 +0700

    style: remove profile comments

--- profiles/umbriel.nix
+++ profiles/umbriel.nix
@@ -1,8 +1,6 @@
 { username, ... }:
 
 {
-  # Optional profile: enable Umbriel for the Home Manager user.
-  # Add "umbriel" to hosts/machine/identity.nix when you want this profile.
   home-manager.users.${username}.imports = [
     ../modules/home-manager/umbriel.nix
   ];
