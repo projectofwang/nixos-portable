e04af529774f579ec52aae3fdae48aa6ff12f80e
projectofwang <projectofwang@gmail.com>
2026-09-11 19:13:37 +0700

    style(media): preserve intentional predicate layout

--- profiles/media.nix
+++ profiles/media.nix
@@ -2,11 +2,9 @@
 
 {
   # Optional entertainment stack. It is intentionally not part of desktop.
-  nixpkgs.config.allowUnfreePredicate =
-    pkg:
-    builtins.elem (lib.getName pkg) [
-      "stremio-linux-shell"
-    ];
+  /*nixfmt:disable*/
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [ "stremio-linux-shell" ];
+  /*nixfmt:enable*/
 
   home-manager.users.${username}.home.packages = with pkgs; [
     mpv
