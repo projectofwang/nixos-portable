d1513447f4224009d068cec3f2c7fae7e4d9448f
projectofwang <projectofwang@gmail.com>
2026-09-11 21:06:53 +0700

    style: remove Nix comments

--- modules/nixos/core/security.nix
+++ modules/nixos/core/security.nix
@@ -1,8 +1,6 @@
 { lib, ... }:
 
 {
-  # Keep this a default so desktop/networking modules may enable polkit when needed.
   security.polkit.enable = lib.mkDefault false;
-
   security.sudo.enable = true;
 }
