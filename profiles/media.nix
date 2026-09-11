95bc72e4bba16eceac3ca401efc84552069239a4
projectofwang <projectofwang@gmail.com>
2026-09-11 18:29:22 +0700

    feat(media): add optional media profile

---
+++ profiles/media.nix
@@ -0,0 +1,16 @@
+{ username, lib, pkgs, ... }:
+
+{
+  # Optional entertainment stack. It is intentionally not part of desktop.
+  nixpkgs.config.allowUnfreePredicate = pkg:
+    builtins.elem (lib.getName pkg) [
+      "stremio-linux-shell"
+    ];
+
+  home-manager.users.${username}.home.packages = with pkgs; [
+    mpv
+    mpvpaper
+    vlc
+    stremio-linux-shell
+  ];
+}
