50322ec613b519aa745b0d8d14c2c5e0c4dbd2bf
projectofwang <projectofwang@gmail.com>
2026-09-11 21:06:37 +0700

    style: remove Nix comments

--- modules/nixos/browser/default.nix
+++ modules/nixos/browser/default.nix
@@ -5,8 +5,6 @@
     ../graphics.nix
   ];
 
-  # Shared Wayland/browser integration. Browser selection lives in the
-  # individual browser profiles so enabling one never installs another.
   environment.sessionVariables = {
     MOZ_ENABLE_WAYLAND = "1";
     NIXOS_OZONE_WL = "1";
