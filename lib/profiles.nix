07f79ff32e330fd7cad5ce3857fe7255c8fa09d7
projectofwang <projectofwang@gmail.com>
2026-09-11 12:55:18 +0700

    fix(lib): scope discovered profiles correctly

--- lib/profiles.nix
+++ lib/profiles.nix
@@ -2,13 +2,14 @@
 
 let
   profileFiles = builtins.readDir ../profiles;
-in
-{
   available = map (name: lib.removeSuffix ".nix" name) (
     lib.filter (name: profileFiles.${name} == "regular" && lib.hasSuffix ".nix" name) (
       builtins.attrNames profileFiles
     )
   );
+in
+{
+  inherit available;
 
   validate =
     selected:
