6e040ac1de06d79727baab8904f51639353c92b9
projectofwang <projectofwang@gmail.com>
2026-09-11 21:09:01 +0700

    style: remove profile comments

--- profiles/mail-thunderbird.nix
+++ profiles/mail-thunderbird.nix
@@ -1,6 +1,5 @@
 { username, pkgs, ... }:
 
 {
-  # Thunderbird is kept optional so a mail client never becomes a desktop dependency.
   home-manager.users.${username}.home.packages = [ pkgs.thunderbird ];
 }
