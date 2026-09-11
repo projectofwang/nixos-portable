275b67d78552ec4d2bc627c43550467ea4c3a22b
projectofwang <projectofwang@gmail.com>
2026-09-11 12:50:44 +0700

    style(lib): format helper entrypoint

--- lib/default.nix
+++ lib/default.nix
@@ -1,8 +1,9 @@
 { inputs, lib, home-manager }:
-
 let
   profiles = import ./profiles.nix { inherit lib; };
-  host = import ./mk-host.nix { inherit inputs lib home-manager profiles; };
+  host = import ./mk-host.nix {
+    inherit inputs lib home-manager profiles;
+  };
 in
 {
   inherit profiles;
