b8cc718c808caa13f20bfacae034497b24e1fbcc
projectofwang <projectofwang@gmail.com>
2026-09-13 03:24:01 +0700

    feat: make CI host explicitly multi-architecture

--- hosts/ci/identity.nix
+++ hosts/ci/identity.nix
@@ -3,6 +3,10 @@
   hostname = "nixos-portable-ci";
   username = "ci";
   system = "x86_64-linux";
+  architectures = [
+    "x86_64-linux"
+    "aarch64-linux"
+  ];
   timeZone = "UTC";
   nixosStateVersion = "26.05";
   homeStateVersion = "26.05";
