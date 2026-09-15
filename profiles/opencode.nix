16b09cac6224acc28a88d6c610e5284e276a1ed4
projectofwang <projectofwang@gmail.com>
2026-09-15 12:17:18 +0700

    Remove stale OpenCode Home Manager profile

--- profiles/opencode.nix
+++
@@ -1,8 +0,0 @@
-{ username, ... }:
-
-{
-  # Install OpenCode through the selected Home Manager user.
-  home-manager.users.${username}.imports = [
-    ../modules/home-manager/opencode.nix
-  ];
-}
