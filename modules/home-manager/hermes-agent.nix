fdaab1afcaaf52dda456bcaa485b42796d7d4b32
projectofwang <projectofwang@gmail.com>
2026-09-19 14:53:04 +0700

    remove hermes-agent module

--- modules/home-manager/hermes-agent.nix
+++
@@ -1,8 +0,0 @@
-{ inputs, pkgs, ... }:
-
-{
-  # Install the pinned Hermes Agent package without changing its provider settings.
-  home.packages = [
-    inputs.hermes-agent.packages.${pkgs.stdenv.hostPlatform.system}.default
-  ];
-}
