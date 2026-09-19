e0ccf76fb5224ff28088e6039c43e979e3171134
projectofwang <projectofwang@gmail.com>
2026-09-19 19:10:20 +0700

    Remove profiles/hermes-agent.nix

--- profiles/hermes-agent.nix
+++
@@ -1,8 +0,0 @@
-{ username, ... }:
-
-{
-  # Install Hermes Agent through the selected Home Manager user.
-  home-manager.users.${username}.imports = [
-    ../modules/home-manager/hermes-agent.nix
-  ];
-}
